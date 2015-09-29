397d1f62f1f3224b35328c8da4e3ecb5227bbc81
crlenz <chris@crlenz.com>
2015-09-29 15:57:51 -0400

    Update createGitRelease.groovy

--- createGitRelease.groovy
+++ createGitRelease.groovy
@@ -4,18 +4,13 @@
 // use job workspace git clone instead of file system repo
 // move this script to github
 
-//def strOriginBranchName = build.buildVariableResolver.resolve("Origin Branch Name")
 def strOriginBranchName = "master"
 def strBuildNumber = build.getNumber()
-//def strReleaseBranchName = "release/$strOriginBranchName"
 def strReleaseBranchName = "release/r$strBuildNumber"
-//def strTagName = strOriginBranchName.toUpperCase()
 def strTagName = "R$strBuildNumber"
 def strOriginRemote = "origin"
 def strReleaseRemote = "release"
-//def strIntermediateRepoPath = "/home/admin/git/main-1/"
 def strIntermediateRepoPath = build.getWorkspace().toURI()
-//def strIntermediateRepoPath = "/var/lib/jenkins/jobs/freestyle-git-release/workspace"
 
 println "\norigin branch: $strOriginBranchName\nrelease branch: $strReleaseBranchName\ntag: $strTagName\nintermediate repo: $strIntermediateRepoPath\n"
 
@@ -28,16 +23,23 @@ def execCommand(command, workingDir) {
   process.waitFor()
   println "   exitValue: ${process.exitValue()}\n   text: ${process.text}\n   err.text: ${process.err.text}"
 }
-/*
 execCommand("git checkout master", workingDir)
 execCommand("git pull $strOriginRemote $strOriginBranchName", workingDir)
-*/
-//execCommand("git branch $strReleaseBranchName", workingDir)
-//execCommand("git tag $strTagName", workingDir)
 
-//execCommand("git remote add release 'https://jenkins-crlenz:system123@github.com/crlenz/release-1.git'", workingDir)
-//execCommand("git remote add release 'git@github.com:crlenz/release-1.git'", workingDir)
-//execCommand("git remote set-url release 'git@github.com:crlenz/release-1.git'", workingDir)
+execCommand("git branch $strReleaseBranchName", workingDir)
+execCommand("git tag $strTagName", workingDir)
 
-//execCommand("git push $strReleaseRemote $strReleaseBranchName", workingDir)
-//execCommand("git push $strReleaseRemote $strTagName", workingDir)
+execCommand("git remote add release 'https://jenkins-crlenz:system123@github.com/crlenz/release-1.git'", workingDir)
+execCommand("git remote add release 'git@github.com:crlenz/release-1.git'", workingDir)
+execCommand("git remote set-url release 'git@github.com:crlenz/release-1.git'", workingDir)
+
+execCommand("git push $strReleaseRemote $strReleaseBranchName", workingDir)
+execCommand("git push $strReleaseRemote $strTagName", workingDir)
+
+// end ****************************************************
+
+//def strOriginBranchName = build.buildVariableResolver.resolve("Origin Branch Name")
+//def strReleaseBranchName = "release/$strOriginBranchName"
+//def strTagName = strOriginBranchName.toUpperCase()
+//def strIntermediateRepoPath = "/home/admin/git/main-1/"
+//def strIntermediateRepoPath = "/var/lib/jenkins/jobs/freestyle-git-release/workspace"
